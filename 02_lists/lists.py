6c71357e45040a7952fc9cd86ab47a806f2229b6
Jose Ángel Barroso <joseangel.barroso@gmail.com>
2019-02-02 13:00:27 +0100

    Adding lists

---
+++ 02_lists/lists.py
@@ -0,0 +1,18 @@
+# Creating a list
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+
+print(mylist)
+
+mylist = [3, 4, 5]
+
+print(mylist)
+
+# Access to an element
+print(mylist[0])
+
+# iteration
+for x in mylist:
+    print(x)
