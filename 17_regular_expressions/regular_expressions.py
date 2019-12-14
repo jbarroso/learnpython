20155613e0518c629b68ebcf6faa8efd1921b387
Jose Ángel Barroso <joseangel.barroso@gmail.com>
2019-12-14 22:01:21 +0100

    WIP: Adding regexp and exceptions

---
+++ 17_regular_expressions/regular_expressions.py
@@ -0,0 +1,28 @@
+import re
+pattern = re.compile(r"\[(on|off)\]")
+print(re.search(pattern, "Mono: Playback 65 [75%] [-16.50dB] [on]"))
+# Returns a Match object!
+print(re.search(pattern, "Nada...:-("))
+# Doesn't return anything.
+# End Example
+
+
+# Exercise: make a regular expression that will match an email
+def test_email(your_pattern):
+    pattern = re.compile(your_pattern)
+    emails = [
+        "john@example.com",
+        "python-list@python.org",
+        "wha.t.`1an?ug{}ly@email.com"
+    ]
+    for email in emails:
+        if not re.match(pattern, email):
+            print("You failed to match %s" % (email))
+        elif not your_pattern:
+            print("Forgot to enter a pattern!")
+        else:
+            print("Pass")
+
+
+pattern = r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?"
+test_email(pattern)
